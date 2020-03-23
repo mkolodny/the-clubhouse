69c879e0770379981f1122932764e09e2d5803f7
Michael Kolodny <michaelckolodny@gmail.com>
2020-03-22 22:20:36 -0700

    Commit frontend build directory

---
+++ frontend/build/precache-manifest.a0ecc1d7ec9089286d1dcca9c56f3c72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe2c9a142a4a95a9ba0e17124cb4052",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b677d3acd24a2b3f16d",
+    "url": "/static/css/main.6a480d3c.chunk.css"
+  },
+  {
+    "revision": "006dee27d394b7795f25",
+    "url": "/static/js/2.3b75df16.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3b75df16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b677d3acd24a2b3f16d",
+    "url": "/static/js/main.8d6ccc0f.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
